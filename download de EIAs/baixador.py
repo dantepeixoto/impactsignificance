0dda787dfa1899a28a799ab3aa4798e6e555a549
Dante Peixoto <dante@evag.me>
2021-12-10 12:41:34 -0300

    cria script para baixar pdfs dos sites escolhidos

---
+++ download de EIAs/baixador.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+import requests
+from bs4 import BeautifulSoup
+
+url = input('Digite a URL do site em que deseja buscar documentos: ')
+
+baixados = set()
+
+pasta = Path('PDFs')
+pasta.mkdir(exist_ok=True)
+
+def salva_pdf(url):
+	print('salvando PDF: ',url)
+	pdf = pasta/url.replace('/', '_')
+	pdf.write_bytes(requests.get(url).content)
+
+def baixador(url):
+	r = requests.get(url)
+	soup = BeautifulSoup(r.text, 'html.parser')
+	for a in soup.find_all('a'):
+		link = requests.compat.urljoin(url, a.get('href'))
+		if link in baixados:
+			continue
+		baixados.add(link)
+		print(link)
+
+		if link.endswith('.pdf'):
+			salva_pdf(link)
+		elif link.endswith('/'):
+			baixador(link)
+
+baixador(url)
