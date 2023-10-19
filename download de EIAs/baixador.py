9c96b6014d1d577832e3e84351807427223856b8
Dante Peixoto <dante@evag.me>
2023-10-19 15:55:10 -0300

    Ignora o que está fora do domínio principal

--- download de EIAs/baixador.py
+++ download de EIAs/baixador.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
 
 url = input('Digite a URL do site em que deseja buscar documentos: ')
 urlprincipal = urlparse(url)
@@ -13,29 +13,29 @@ pasta = Path('download')
 pasta.mkdir(exist_ok=True)
 
 def salva_pdf(url):
-	print('salvando PDF: ',url)
-	pdf = pasta/url.replace('https://', '').replace('http://', '')
-	pdf.parent.mkdir(parents=True, exist_ok=True)
-	pdf.write_bytes(requests.get(url).content)
+    print('salvando PDF: ', url)
+    pdf = pasta / url.replace('https://', '').replace('http://', '')
+    pdf.parent.mkdir(parents=True, exist_ok=True)
+    pdf.write_bytes(requests.get(url).content)
 
 def baixador(url):
-	r = requests.get(url)
-	soup = BeautifulSoup(r.text, 'html.parser')
-	for a in soup.find_all('a'):
-		link = requests.compat.urljoin(url, a.get('href'))
-		if link in baixados:
-			continue
-		if urlprincipal.netloc and urlprincipal.path not in link:
-			continue
-		baixados.add(link)
-		# log #
-		print(link)
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    for a in soup.find_all('a'):
+        link = urljoin(url, a.get('href'))
+        if link in baixados:
+            continue
+        link_netloc = urlparse(link).netloc
+        if urlprincipal.netloc not in link_netloc:
+            continue
+        baixados.add(link)
+        # log #
+        print(link)
 
-
-		if urlprincipal.netloc and urlprincipal.path in link:
-			if link.endswith('.pdf'):
-				salva_pdf(link)
-			elif link.endswith('/'):
-				baixador(link)
+        if urlprincipal.netloc in link_netloc:
+            if link.endswith('.pdf'):
+                salva_pdf(link)
+            elif link.endswith('/'):
+                baixador(link)
 
 baixador(url)
